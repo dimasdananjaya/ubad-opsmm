5e94a2e9b4f0d5895a966b000ee9e0b4f8256d32
dimasdananjaya <gededimas@gmail.com>
2020-06-14 22:10:08 +0800

    controller, database, model preparation and index page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,36 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id_user');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status');
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('periode', function (Blueprint $table) {
+            $table->bigIncrements('id_periode');
+            $table->string('periode');
+            $table->string('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('dana_operasional', function (Blueprint $table) {
+            $table->bigIncrements('id_dana_operasional');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_periode');
+            $table->string('nama_dana');
+            $table->string('penanggung_jawab');
+            $table->string('file');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('id_periode')->references('id_periode')->on('periode');
+            $table->foreign('id_user')->references('id_user')->on('users');   
         });
     }
 
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<section id="index-page">
+    <div class="container">
+        <div class="card d-block mx-auto">
+            <div class="card-header">
+                <img class="card-img img-responsive d-block mx-auto" src="{{asset('resources/logo/index.svg')}}">
+            </div>
+            <div class="card-body text-center">
+                <div class="card-text">
+                    <h4><b>Ubad Operasional Money Management System</b></h4>
+                    <p><small>login to continue</small></p>
+                    <a class="btn btn-primary round-btn" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>    
+@endsection
